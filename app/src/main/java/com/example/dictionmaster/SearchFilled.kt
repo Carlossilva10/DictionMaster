448e05835f5403ee3dc15897e405bdf0b0080681
Carlos Silva <caduaba@hotmail.com>
2024-01-18 14:08:17 -0300

    iniciando o App

---
+++ app/src/main/java/com/example/dictionmaster/SearchFilled.kt
@@ -0,0 +1,11 @@
+package com.example.dictionmaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchFilled : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_filled)
+    }
+}
